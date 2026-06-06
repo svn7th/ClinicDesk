e5175b87255cc59cc436c8afac8808d0a7a352a9
svn7th <sefan7747@gmail.com>
2026-06-06 18:24:45 +0430

    Delete config directory

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("APP_NAME", "ClinicDesk");
-define("BASE_URL", "http://localhost/web2/ClinicDesk/");
-define("ITEMS_PER_PAGE", 10);
-
-define("MAX_AVATAR_SIZE", 1024 * 1024); // 1 MB
-define("MAX_PDF_SIZE", 3 * 1024 * 1024); // 3 MB
--- config/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("DB_HOST", "localhost");
-define("DB_NAME", "clinicdesk_db");
-define("DB_USER", "root");
-define("DB_PASS", "");
